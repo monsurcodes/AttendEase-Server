0a353af441c487f8b27ea353fba7f3193e455b57
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-09 07:26:49 +0530

    refactor: name from create to createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,6 @@ export class UserController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    return this.userService.createUser(createUserDto);
   }
 }
