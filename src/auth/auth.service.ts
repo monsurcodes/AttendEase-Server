de257b95efa6712809f2322417b6d994d35d47db
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-09 07:19:32 +0530

    feat: add register user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  register() {
+  async register(registerDto: RegisterDto) {
+    // exisiting user check
+    const existingUser = await this.userService.findByEmail(registerDto.email);
+    if (existingUser) {
+      throw new ConflictException('User with this email already exists.');
+    }
+
+    const { password, ...registerDtoWithoutPassword } = registerDto;
+
+    // password hash
+    const saltOrRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltOrRounds);
+
+    // creating a new user
+    const user = await this.userService.createUser({
+      ...registerDtoWithoutPassword,
+      passwordHash,
+    });
+
     return {
       message: 'User registered!',
+      user,
     };
   }
 
