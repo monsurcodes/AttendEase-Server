1dfe4ecd289340355426314a6fd4f3b5d3490d9d
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-13 15:42:10 +0530

    feat: better-auth config file

---
+++ src/auth/auth.config.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { betterAuth } from 'better-auth';
+import { bearer } from 'better-auth/plugins';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [bearer()],
+  hooks: {},
+  trustedOrigins: [],
+  advanced: {
+    disableOriginCheck: true, // <-- Make sure to remove this in prod
+    useSecureCookies: true,
+  },
+});
