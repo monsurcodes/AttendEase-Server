616cc11eb091ef9bc1f5e5c541aa0ab5fe0ff420
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-13 15:43:17 +0530

    feat: replace jwt config with better-auth config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
-import { UserModule } from '../user/user.module';
+import { AuthModule as NestjsBetterAuthModule } from '@thallesp/nestjs-better-auth';
+import { auth } from './auth.config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+    NestjsBetterAuthModule.forRoot({
+      auth,
+      bodyParser: {
+        json: { limit: '2mb' },
+        urlencoded: { limit: '2mb', extended: true },
+        rawBody: true,
+      },
     }),
-    UserModule,
   ],
-  controllers: [AuthController],
-  providers: [AuthService],
-  exports: [AuthService],
+  exports: [NestjsBetterAuthModule],
 })
 export class AuthModule {}
