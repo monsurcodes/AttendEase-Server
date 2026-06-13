a5b3feefdad817e658c748db653f6c52f399c6f1
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-13 15:46:36 +0530

    refactor: allow unauthorized acces to this endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
+  @AllowAnonymous()
   getHealth() {
     return this.appService.getHealth();
   }
