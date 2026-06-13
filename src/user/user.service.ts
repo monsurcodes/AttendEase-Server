c768ae80e3147db01525d46ec5a44abdf86e182a
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-13 15:45:28 +0530

    refactor: explicit createUser method not required as better-auth is being used

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
   constructor(private prismaService: PrismaService) {}
-
-  async createUser(createUserDto: CreateUserDto) {
-    return await this.prismaService.user.create({
-      data: createUserDto,
-    });
-  }
 
   async findByEmail(email: string) {
     return await this.prismaService.user.findUnique({ where: { email } });
