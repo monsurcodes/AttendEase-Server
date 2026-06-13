136d67bfa5ac70c050446b8d76c46e2ef4c2ba30
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-13 15:38:39 +0530

    feat: disbale bodyParser for better-auth to work

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bodyParser: false, // <-- REQUIRED FOR BETTER AUTH
+  });
 
   app.setGlobalPrefix('api', {
     exclude: ['health'],
