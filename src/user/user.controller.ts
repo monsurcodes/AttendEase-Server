f738cd79a01c208526c15ec41127ab904044daa5
Monsur Mazumder <monsurcodes@gmail.com>
2026-06-14 08:44:42 +0530

    feat: get endpoint to fetch auth user data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Session, type UserSession } from '@thallesp/nestjs-better-auth';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getProfile(@Session() session: UserSession) {
+    return this.userService.getProfile(session);
+  }
 }
