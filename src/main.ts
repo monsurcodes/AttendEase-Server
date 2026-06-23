0d7321e1fddb17e02ee07f0adcb29c9d0272618f
Minkxx <monsurcodes@gmail.com>
2026-06-23 22:59:35 +0530

    feat: add redis adapter to websocket

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
+import { RedisIoAdapter } from './redis/redis-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,10 @@ async function bootstrap() {
   app.setGlobalPrefix('api', {
     exclude: ['/', 'health'],
   });
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
 
   const adapterHost = app.get(HttpAdapterHost);
 
